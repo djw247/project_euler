19af788d8f9ca7176776a54b0c2cf47d24be0029
djw247 <109682708+djw247@users.noreply.github.com>
2023-03-21 00:33:28 +0800

    Add files via upload

--- problem_01.js
+++ problem_01.js
@@ -1,6 +1,12 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 // Find the sum of all the multiples of 3 or 5 below 1000.
+
+/*
+declare integer variable sum = 0
+create for loop to run through values 0 to 1,000
+for each value, IF divisible by 3 OR 5, add value to sum
+console log sum to find sum of all multiples of 3 or 5 below 1,000
+*/
 
 let sum = 0
 
--- problem_06.js
+++ problem_06.js
@@ -6,6 +6,19 @@
 // 3025 - 285 = 2640.
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+/*
+declare integer variables sum = 0, addedSrs = 0, n = 0
+
+create while loop for WHILE n is greater than 0, starting at 100 and reducing by 1 each loop
+
+in the loop:
+add each value of n to sum
+add square of each value to addedSqrs
+reduce value of n by 1
+
+console log squared value of sum MINUS the total of addedSqrs to find the result
+*/
+
 let sum = 0
 let addedSqrs = 0
 let n = 100
